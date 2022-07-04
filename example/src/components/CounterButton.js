1caac6c792d4d87eee8b218f7e6a475440efdd0f
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-07-04 14:47:44 -0700

    Refactor counter button styles into a static property

--- example/src/components/CounterButton.js
+++ example/src/components/CounterButton.js
@@ -1,42 +1,45 @@
 class CounterButton extends HTMLElement {
+  static styles = /* css */ `
+    button {
+      font-family: inherit;
+      font-size: inherit;
+      background-color: #efefef;
+      border: 2px solid #000;
+      border-radius: 8px;
+      padding: 6px 10px;
+    }
+    button:hover,
+    button:focus {
+      cursor: pointer;
+      outline: none;
+      background-color: #e6e6e6;
+    }
+  `;
+
   constructor() {
     super();
-    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this.shadow = this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
-    this._render();
-    this.count = this._shadowRoot.querySelector('#count');
-    const counterButton = this._shadowRoot.querySelector('button');
-    counterButton.addEventListener('click', this._increment.bind(this));
+    this.render();
+    this.count = this.shadow.querySelector('#count');
+    const counterButton = this.shadow.querySelector('button');
+    counterButton.addEventListener('click', this.increment.bind(this));
   }
 
-  _render() {
+  render() {
     const template = document.createElement('template');
-    template.innerHTML = /*html*/ `
+    template.innerHTML = /* html */ `
       <style>
-        button {
-          font-family: inherit;
-          font-size: inherit;
-          background-color: #efefef;
-          border: 2px solid #000;
-          border-radius: 8px;
-          padding: 6px 10px;
-        }
-        button:hover,
-        button:focus {
-          cursor: pointer;
-          outline: none;
-          background-color: #e6e6e6;
-        }
+        ${CounterButton.styles}
       </style>
       <button>Clicked <span id="count">0</span> times</button>
     `;
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadow.appendChild(template.content.cloneNode(true));
   }
 
-  _increment(e) {
-    e.preventDefault();
+  increment() {
     this.count.textContent = Number(this.count.textContent) + 1;
   }
 }
